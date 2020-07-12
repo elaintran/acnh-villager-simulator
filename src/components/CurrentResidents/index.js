44c7d45f23e3e3f37681675ce9fd6c176040e53d
elaintran <elain.ttran@gmail.com>
2020-07-11 20:23:39 -0500

    set up initial autocomplete

--- src/components/CurrentResidents/index.js
+++ src/components/CurrentResidents/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+// import Autocomplete from "react-autocomplete";
 import SearchBar from "../SearchBar";
 // import VillagerTab from "../VillagerTab";
 // import API from "../../utils/API.js";
@@ -8,21 +9,42 @@ class CurrentResidents extends Component {
     state = {
         query: "",
         allVillagers: this.props.villagers,
+        villagerNames: [],
         currentResidents: []
+    }
+
+    componentDidMount() {
+        this.getVillagerNames();
     }
 
     componentDidUpdate(prevProps) {
         if (this.props.villagers !== prevProps.villagers) {
-            this.setState({ allVillagers: this.props.villagers});
+            console.log(prevProps.villagers);
+            this.setState({ allVillagers: this.props.villagers}, () => this.getVillagerNames());
         }
     }
 
-    findResidents = query => {
-        for (let i = 0; i < this.state.allVillagers.length; i++) {
-            if (query === this.state.allVillagers[i].name) {
-                console.log(this.state.allVillagers[i]);
+    getVillagerNames = () => {
+        let villagerNames = [];
+        if (this.state.allVillagers.length !== 0) {
+            for (let i = 0; i < this.state.allVillagers.length; i++) {
+                villagerNames.push(this.state.allVillagers[i].name);
             }
         }
+        this.setState({ villagerNames: villagerNames });
+    }
+
+    findResidents = query => {
+        // let name = query.replace(/\s/g, '').toLowerCase().split(" ");
+        // let newName = name[0].charAt(0).toUpperCase() + name[0].slice(1);
+        // if (newName === "O'hare") {
+        //     newName = "O'Hare";
+        // }
+        // for (let i = 0; i < this.state.allVillagers.length; i++) {
+        //     if (newName === this.state.allVillagers[i].name) {
+        //         console.log(this.state.allVillagers[i]);
+        //     }
+        // }
     }
 
     handleInput = event => {
@@ -37,9 +59,10 @@ class CurrentResidents extends Component {
 
     render() {
         return (
-            <div>
-                <SearchBar change={this.handleInput} submit={this.handleSubmit}>Enter current residents...</SearchBar>
-            </div>
+            <SearchBar
+                options={this.state.villagerNames}
+            />
+                /* <SearchBar change={this.handleInput} submit={this.handleSubmit}>Enter current residents...</SearchBar> */
         );
     }
 }
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,13 +1,128 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import "./style.css";
 
-function SearchBar(props) {
-    return (
-        <form className="search" onSubmit={props.submit}>
-            <i className="fas fa-search"></i>
-            <input type="text" name="query" placeholder={props.children} onChange={props.change} required />
-        </form>
-    );
+// function SearchBar(props) {
+//     return (
+//         <form className="search" onSubmit={props.submit}>
+//             <i className="fas fa-search"></i>
+//             <input type="text" name="query" placeholder={props.children} onChange={props.change} required />
+//         </form>
+//     );
+// }
+
+class SearchBar extends Component {
+    static propTypes = {
+        options: PropTypes.instanceOf(Array).isRequired
+    };
+
+    state = {
+        activeOption: 0,
+        filteredOptions: [],
+        showOptions: false,
+        userInput: ""
+    }
+
+    onChange = event => {
+        const { options } = this.props;
+        const userInput = event.currentTarget.value;
+        const filteredOptions = options.filter((optionName) => 
+            optionName.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+        );
+
+        this.setState({
+            activeOption: 0,
+            filteredOptions,
+            showOptions: true,
+            userInput: event.currentTarget.value
+        });
+    }
+
+    onClick = event => {
+        this.setState({
+            activeOption: 0,
+            filteredOptions: [],
+            showOptions: false,
+            userInput: event.currentTarget.innerText
+        });
+    };
+
+    onKeyDown = event => {
+        const { activeOption, filteredOptions } = this.state;
+    
+        if (event.keyCode === 13) {
+            this.setState({
+                activeOption: 0,
+                showOptions: false,
+                userInput: filteredOptions[activeOption]
+            });
+        } else if (event.keyCode === 38) {
+            if (activeOption === 0) {
+                return;
+            }
+            this.setState({ activeOption: activeOption - 1 });
+        } else if (event.keyCode === 40) {
+            if (activeOption === filteredOptions.length - 1) {
+                console.log(activeOption);
+                return;
+            }
+            this.setState({ activeOption: activeOption + 1 });
+        }
+    };    
+
+    render() {
+        const {
+            onChange,
+            onClick,
+            onKeyDown,
+            state: {
+                activeOption,
+                filteredOptions,
+                showOptions,
+                userInput }
+            } = this;
+        let optionList;
+        if (showOptions && userInput) {
+            if (filteredOptions.length) {
+                optionList = (
+                    <ul className="options">
+                        {filteredOptions.map((optionName, index) => {
+                            let className;
+                            if (index === activeOption) {
+                                className = 'option-active';
+                            }
+                            return (
+                                <li className={className} key={optionName} onClick={onClick}>
+                                    {optionName}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                );
+            } else {
+                optionList = (
+                    <div className="no-options">
+                        <em>No Option!</em>
+                    </div>
+                );
+            }
+        }
+        return (
+            <React.Fragment>
+                <div className="search">
+                    <input
+                        type="text"
+                        className="search-box"
+                        onChange={onChange}
+                        onKeyDown={onKeyDown}
+                        value={userInput}
+                    />
+                    <input type="submit" value="" className="search-btn" />
+                </div>
+                {optionList}
+            </React.Fragment>
+        );
+    }
 }
 
 export default SearchBar;
